43a43f9037e61f0472bb1233024a3274b3d41302
dalibor.stys <dalibor.stys@synchronics.at>
2019-10-05 08:42:51 +0200

    Initial commit

---
+++ A9MTE_Stys/A9MTE_Stys/A9MTE_Stys/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace A9MTE_Stys.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
