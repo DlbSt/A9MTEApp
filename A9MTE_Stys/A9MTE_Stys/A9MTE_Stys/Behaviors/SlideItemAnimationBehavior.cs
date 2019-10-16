98d9249003664bfd94e0da1cf16e8f02320a6d43
dalibor.stys <dalibor.stys@synchronics.at>
2019-10-16 11:58:43 +0200

    Animations

---
+++ A9MTE_Stys/A9MTE_Stys/A9MTE_Stys/Behaviors/SlideItemAnimationBehavior.cs
@@ -0,0 +1,61 @@
+﻿using Prism.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
+
+namespace A9MTE_Stys.Behaviors
+{
+    public class SlideItemAnimationBehavior : BehaviorBase<PancakeView>
+    {
+        protected override void OnAttachedTo(PancakeView bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                if (bindable.GestureRecognizers[1] is SwipeGestureRecognizer)
+                {
+                    ((SwipeGestureRecognizer)bindable.GestureRecognizers[1]).Swiped += SlideItemAnimationBehavior_Swiped;
+                }
+            }
+        }
+
+        protected override void OnDetachingFrom(PancakeView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                if (bindable.GestureRecognizers[1] is SwipeGestureRecognizer)
+                {
+                    ((SwipeGestureRecognizer)bindable.GestureRecognizers[1]).Swiped -= SlideItemAnimationBehavior_Swiped;
+                }
+            }
+        }
+
+        private void SlideItemAnimationBehavior_Swiped(object sender, SwipedEventArgs e)
+        {
+            var trX = ((PancakeView)sender).TranslationX;
+            var trY = ((PancakeView)sender).TranslationY;
+
+            if (e.Direction == SwipeDirection.Left)
+            {
+                ((PancakeView)sender).TranslateTo(trX - 200, trY, 500);
+            }
+            else if (e.Direction == SwipeDirection.Right)
+            {
+                ((PancakeView)sender).TranslateTo(trX + 200, trY, 500);
+            }
+            else if (e.Direction != SwipeDirection.Up && e.Direction != SwipeDirection.Down)
+            {
+                ((PancakeView)sender).TranslateTo(trX + 200, trY, 500);
+
+                Task.Delay(500);
+            }
+        }
+
+    }
+}
---
+++ A9MTE_Stys/A9MTE_Stys/A9MTE_Stys/Behaviors/TappedItemAnimationBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
+
+namespace A9MTE_Stys.Behaviors
+{
+    public class TappedItemAnimationBehavior : BehaviorBase<PancakeView>
+    {
+        protected override void OnAttachedTo(PancakeView bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            if (bindable.GestureRecognizers[0] is TapGestureRecognizer)
+            {
+                ((TapGestureRecognizer)bindable.GestureRecognizers[0]).Tapped += TappedItemAnimationBehavior_Tapped;
+            }
+            
+        }
+
+        protected override void OnDetachingFrom(PancakeView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            if (bindable.GestureRecognizers[0] is TapGestureRecognizer)
+            {
+                ((TapGestureRecognizer)bindable.GestureRecognizers[0]).Tapped -= TappedItemAnimationBehavior_Tapped;
+            }
+        }
+
+        private async void TappedItemAnimationBehavior_Tapped(object sender, EventArgs e)
+        {
+            var scale = ((PancakeView)sender).Scale;
+
+            await ((PancakeView)sender).ScaleTo(scale * 0.9, 100);//FadeTo(0.3, 10);
+            await ((PancakeView)sender).ScaleTo(scale, 100);//FadeTo(1, 500);
+        }
+    }
+}
